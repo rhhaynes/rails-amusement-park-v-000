6d4d2ff0d2f7eed7d86d3a8917a73bd8ab54863c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-01 15:54:06 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -9,9 +9,6 @@ class RidesController < ApplicationController
   private
   
   def ride_params
-    params.require(:ride).permit(
-      :user_id,
-      :attraction_id
-      )
+    params.require(:ride).permit(:user_id, :attraction_id)
   end
 end
